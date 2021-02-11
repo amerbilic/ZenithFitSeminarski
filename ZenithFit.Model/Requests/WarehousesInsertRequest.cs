12b2481add29ef48895f33fa1d4cb3d8469a5ed7
amerbilic <bilic.amer@outlook.com>
2021-02-11 13:09:27 +0100

    commit

--- ZenithFit.Model/Requests/WarehousesInsertRequest.cs
+++ ZenithFit.Model/Requests/WarehousesInsertRequest.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ZenithFit.Model.Requests
 {
     public class WarehousesInsertRequest
     {
+        [Required]
+        [MinLength(4)]
         public string WarehouseName { get; set; }
+        [Required]
+        [MinLength(4)]
         public string WarehouseAddress { get; set; }
         public string WarehouseNote { get; set; }
     }
